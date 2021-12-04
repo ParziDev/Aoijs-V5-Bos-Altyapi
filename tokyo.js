35c0f99f44ea934b51e05a5d95787e4454b3f4c6
Parzi <86732697+ParziDev@users.noreply.github.com>
2021-12-05 01:24:48 +0300

    Create tokyo.js

---
+++ tokyo.js
@@ -0,0 +1,38 @@
+const aoijs = require("aoi.js")
+const loader = new aoijs.LoadCommands(bot)
+const bot = new aoijs.Bot({
+token: process.env.token, 
+prefix: "$getServerVar[prefix]", 
+intents: "all" 
+})loader.load(bot.cmd,"./komutlar/")
+
+
+loader.load(bot.cmd,"./komutlar/")
+
+//Callbackler
+bot.joinCommand()
+bot.leaveCommand()
+bot.onMessage()
+
+//Durum
+bot.status({
+text:"Tokyo Code",
+type:"PLAYING",
+status:"dnd",
+time: 12
+})
+
+//Variableler
+bot.variables({
+prefix:".",
+})
+
+//Komutlar
+bot.command({
+name:"ping",
+aliases:"gecikme",
+description:"Botun pingini gösterir.",
+code:`
+Pong! \`$botPingms\`
+`
+}) //Main komut taslağı
